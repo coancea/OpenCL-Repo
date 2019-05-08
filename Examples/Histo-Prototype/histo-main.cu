9fcef04f0b0938d9b05ee13c128c0a6a2895de66
Cosmin E. Oancea <coancea76@yahoo.com>
2019-05-08 22:50:23 +0200

    prototyping histograms in cuda

---
+++ Examples/Histo-Prototype/histo-main.cu
@@ -0,0 +1,155 @@
+#include <cuda_runtime.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h> 
+
+#define BLOCK       1024
+#define GPU_RUNS    300
+#define CPU_RUNS    1
+
+#define INP_LEN     50000000
+#define Hmax        100000
+
+#define RACE_FACT   8
+
+#define DEBUG_INFO  0
+
+cudaDeviceProp prop;
+unsigned int HWD;
+unsigned int SH_MEM_SZ;
+unsigned int BLOCK_SZ;
+
+#define NUM_THREADS(n)  min(n, HWD)
+
+#include "histo-kernels.cu.h"
+#include "histo-wrap.cu.h"
+
+
+/////////////////////////////////////////////////////////
+// Program main
+/////////////////////////////////////////////////////////
+
+int optimSubHistoDeg(const AtomicPrim prim_kind, const int Q, const int H) {
+    const int el_size = (prim_kind == XCHG)? 2*sizeof(int) : sizeof(int);
+    const int m = ((Q*4 / el_size) * BLOCK) / H;
+    const int coop = (BLOCK + m - 1) / m;
+    printf("COOP LEVEL: %d, subhistogram degree: %d\n", coop, m);
+    return m;
+}
+
+int main() {
+    // set seed for rand()
+    srand(2006);
+
+    { // 0. querry the hardware
+        int nDevices;
+        cudaGetDeviceCount(&nDevices);
+  
+        cudaGetDeviceProperties(&prop, 0);
+        HWD = prop.maxThreadsPerMultiProcessor * prop.multiProcessorCount;
+        BLOCK_SZ = prop.maxThreadsPerBlock;
+        SH_MEM_SZ = prop.sharedMemPerBlock;
+        if (DEBUG_INFO) {
+            printf("Device name: %s\n", prop.name);
+            printf("Number of hardware threads: %d\n", HWD);
+            printf("Block size: %d\n", BLOCK_SZ);
+            printf("Shared memory size: %d\n", SH_MEM_SZ);
+            puts("====");
+        }
+    }
+
+ 
+    // 1. allocate host memory for input and histogram
+    const unsigned int mem_size_input = sizeof(int) * INP_LEN;
+    int* h_input = (int*) malloc(mem_size_input);
+    const unsigned int mem_size_histo = sizeof(int) * Hmax;
+    int* h_histo = (int*) malloc(mem_size_histo);
+ 
+    // 2. initialize host memory
+    randomInit(h_input, INP_LEN);
+    zeroOut(h_histo, Hmax);
+    
+    // 3. allocate device memory
+    int* d_input;
+    int* d_histo;
+    cudaMalloc((void**) &d_input, mem_size_input);
+    cudaMalloc((void**) &d_histo, mem_size_histo);
+ 
+    // 4. copy host memory to device
+    cudaMemcpy(d_input, h_input, mem_size_input, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_histo, h_histo, mem_size_histo, cudaMemcpyHostToDevice);
+ 
+#if 0
+    { // 5. compute a bunch of histograms
+        const int H = 128;
+        
+        unsigned long tm_seq = goldSeqHisto(INP_LEN, H, h_input, h_histo);
+        printf("Histogram Sequential        took: %lu microsecs\n", tm_seq);
+
+        int histos_per_block = BLOCK/32;
+        //int histos_per_block = optimSubHistoDeg(CAS, 12, H); 
+        unsigned long tm_add = locMemHwdAddCoop(ADD, INP_LEN, H, histos_per_block, d_input, h_histo);
+        printf("Histogram Local-Mem ADD with subhisto-degree %d took: %lu microsecs\n", histos_per_block, tm_add);
+
+        unsigned long tm_cas = locMemHwdAddCoop(CAS, INP_LEN, H, histos_per_block, d_input, h_histo);
+        printf("Histogram Local-Mem CAS with subhisto-degree %d took: %lu microsecs\n", histos_per_block, tm_cas);
+
+        //coop = optimalCoop(XCHG, 12, H);
+        unsigned long tm_xch = locMemHwdAddCoop(XCHG, INP_LEN, H, histos_per_block/2, d_input, h_histo);
+        printf("Histogram Local-Mem XCG with subhisto-degree %d took: %lu microsecs\n", histos_per_block, tm_xch);
+    }
+#endif
+
+    { // 5. Dataset: Local Memory:
+        const int num_histos = 5;
+        const int num_m_degs = 5;
+        const int histo_sizes[num_histos] = { 32, 64, 128, 256, 512 }; //{ 64, 128, 256, 512 };
+        //const AtomicPrim atomic_kinds[3] = {ADD, CAS, XCHG};
+
+        unsigned long runtimes[3][num_histos][num_m_degs];
+
+        for(int i=0; i<num_histos; i++) {
+            const int H = histo_sizes[i];
+            const int m_opt = optimSubHistoDeg(ADD, 12, H);
+
+            const int min_HB = min(H,BLOCK);
+            const int subhisto_degs[5] = { m_opt, (8*BLOCK) / min_HB, (4*BLOCK) / min_HB, (1*BLOCK) / min_HB, 1};
+
+            goldSeqHisto(INP_LEN, H, h_input, h_histo);
+
+            for(int j=0; j<num_m_degs; j++) {
+                const int histos_per_block = subhisto_degs[j];
+                runtimes[0][i][j] = locMemHwdAddCoop(ADD, INP_LEN, H, histos_per_block, d_input, h_histo);
+                runtimes[1][i][j] = locMemHwdAddCoop(CAS, INP_LEN, H, histos_per_block, d_input, h_histo);
+                runtimes[2][i][j] = locMemHwdAddCoop(XCHG, INP_LEN, H, max(histos_per_block/2,1), d_input, h_histo);
+            }
+        }
+        
+        for(int k=0; k<3; k++) {
+            if     (k==0) printf("LOC_ADD\t");
+            else if(k==1) printf("LOC_CAS\t");
+            else if(k==2) printf("LOC_XCG\t");
+
+            for(int i = 0; i<num_histos; i++) { printf("H=%d\t", histo_sizes[i]); }
+            printf("\n");
+            for(int j=0; j<num_m_degs; j++) {
+                printf("SH_DEG_%d\t", j);
+                for(int i = 0; i<num_histos; i++) {
+                    printf("%lu\t", runtimes[k][i][j]);
+                }
+                printf("\n");
+            }
+            printf("\n");
+        }
+    }
+
+
+    // 7. clean up memory
+    free(h_input);
+    free(h_histo);
+    cudaFree(d_input);
+    cudaFree(d_histo);
+}
+
---
+++ Examples/Histo-Prototype/histo-wrap.cu.h
@@ -0,0 +1,142 @@
+#ifndef HISTO_WRAPPER
+#define HISTO_WRAPPER
+
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+{
+    unsigned int resolution=1000000;
+    long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
+    result->tv_sec = diff / resolution;
+    result->tv_usec = diff % resolution;
+    return (diff<0);
+}
+
+void randomInit(int* data, int size) {
+    for (int i = 0; i < size; ++i)
+        data[i] = rand(); // (float)RAND_MAX;
+}
+
+void zeroOut(int* data, int size) {
+    for (int i = 0; i < size; ++i)
+        data[i] = 0;
+}
+
+template<class T>
+bool validate(T* A, T* B, unsigned int sizeAB) {
+    for(int i = 0; i < sizeAB; i++) {
+        if (A[i] != B[i]) {
+            printf("INVALID RESULT %d %d %d\n", i, A[i], B[i]);
+            return false;
+        }
+    }
+    //printf("VALID RESULT!\n");
+    return true;
+}
+
+void computeSeqHisto(const int N, const int H, int* input, int* histo) {
+    for(int i=0; i<N; i++) {
+        struct indval<int> iv = f<int>(input[i], H);
+        histo[iv.index] += iv.value;
+    }
+}
+
+unsigned long
+goldSeqHisto(const int N, const int H, int* input, int* histo) {
+    unsigned long int elapsed;
+    struct timeval t_start, t_end, t_diff;
+    gettimeofday(&t_start, NULL);
+
+    for(int q=0; q<CPU_RUNS; q++) {
+        zeroOut(histo, H);
+        computeSeqHisto(N, H, input, histo);
+    }
+
+    gettimeofday(&t_end, NULL);
+    timeval_subtract(&t_diff, &t_end, &t_start);
+    elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec); 
+    //printf("Sequential Naive version runs in: %lu microsecs\n", elapsed);
+    return (elapsed / CPU_RUNS); 
+}
+
+unsigned long
+locMemHwdAddCoop(AtomicPrim select, const int N, const int H, const int histos_per_block, int* d_input, int* h_ref_histo) {
+    if(histos_per_block <= 0) {
+        printf("Illegal subhistogram degree: %d, H:%d, XCG?=%d, EXITING!\n", histos_per_block, H, (select==XCHG));
+        exit(0);
+    }
+
+    // setup execution parameters
+    const size_t num_blocks = (NUM_THREADS(N) + BLOCK - 1) / BLOCK; 
+    const size_t shmem_size = histos_per_block * H * sizeof(int);
+
+    const size_t mem_size_histo  = H * sizeof(int);
+    const size_t mem_size_histos = histos_per_block * num_blocks * mem_size_histo;
+    int* d_histos;
+    int* d_histo;
+    int* h_histo = (int*)malloc(H*sizeof(int));
+    cudaMalloc((void**) &d_histos, mem_size_histos);
+    cudaMalloc((void**) &d_histo,  H * sizeof(int));
+
+    printf("multi-histogram degree: %d, H:%d\n", histos_per_block, H);
+
+    { // dry run
+      if(select == ADD) {
+        locMemHwdAddCoopKernel<ADD><<< num_blocks, BLOCK, shmem_size >>>
+            (N, H, histos_per_block, NUM_THREADS(N), d_input, d_histos);
+      } else if (select == CAS){
+        locMemHwdAddCoopKernel<CAS><<< num_blocks, BLOCK, shmem_size >>>
+            (N, H, histos_per_block, NUM_THREADS(N), d_input, d_histos);
+      } else { // select == XCHG
+        locMemHwdAddCoopKernel<XCHG><<< num_blocks, BLOCK, 2*shmem_size >>>
+            (N, H, histos_per_block, NUM_THREADS(N), d_input, d_histos);
+      }
+    }
+
+    cudaMemset(d_histos, 0, mem_size_histos);
+    cudaMemset(d_histo , 0, mem_size_histo );
+
+    unsigned long int elapsed;
+    struct timeval t_start, t_end, t_diff;
+    gettimeofday(&t_start, NULL); 
+
+    for(int q=0; q<GPU_RUNS; q++) {
+      //cudaMemset(d_histos, 0, mem_size_histos);
+      if(select == ADD) {
+        locMemHwdAddCoopKernel<ADD><<< num_blocks, BLOCK, shmem_size >>>
+            (N, H, histos_per_block, NUM_THREADS(N), d_input, d_histos);
+      } else if (select == CAS){
+        locMemHwdAddCoopKernel<CAS><<< num_blocks, BLOCK, shmem_size >>>
+            (N, H, histos_per_block, NUM_THREADS(N), d_input, d_histos);
+      } else { // select == XCHG
+        locMemHwdAddCoopKernel<XCHG><<< num_blocks, BLOCK, 2*shmem_size >>>
+            (N, H, histos_per_block, NUM_THREADS(N), d_input, d_histos);
+      }
+    }
+    cudaThreadSynchronize();
+
+    gettimeofday(&t_end, NULL);
+    timeval_subtract(&t_diff, &t_end, &t_start);
+    elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec);
+
+    { // reduce across histograms and copy to host
+        const size_t num_blocks_red = (H + BLOCK - 1) / BLOCK;
+        naive_reduce_kernel<<< num_blocks_red, BLOCK >>>(d_histos, d_histo, H, histos_per_block*num_blocks);
+        cudaMemcpy(h_histo, d_histo, mem_size_histo, cudaMemcpyDeviceToHost);
+    }
+
+    { // validate and free memory
+        bool is_valid = validate<int>(h_histo, h_ref_histo, H);
+
+        free(h_histo);
+        cudaFree(d_histos);
+        cudaFree(d_histo);
+
+        if(!is_valid) {
+            printf("locMemHwdAddCoop: Validation FAILS! Exiting!\n\n");
+            exit(1);
+        }
+    }
+
+    return (elapsed/GPU_RUNS);
+}
+
+#endif // HISTO_WRAPPER
