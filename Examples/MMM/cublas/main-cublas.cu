735328708819b46ef8857491d3d64a1dc8886c48
Cosmin Oancea <cosmin.oancea@diku.dk>
2022-02-08 01:40:08 +0100

    minor

--- Examples/MMM/cublas/main-cublas.cu
+++ Examples/MMM/cublas/main-cublas.cu
@@ -62,27 +62,24 @@ void gpu_blas_mmul_rep(const float *A, const float *B, float *C, const int m, co
      cublasHandle_t handle;
      cublasCreate(&handle);
  
-     printf("AAAAA\n");
-
       unsigned long int elapsed;
       struct timeval t_start, t_end, t_diff;
       gettimeofday(&t_start, NULL); 
      
      // Do the actual multiplication
-      //for(int i=0; i < GPU_RUNS; i++) {
+      for(int i=0; i < GPU_RUNS; i++) {
          cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
-      //}
+      }
+
       gettimeofday(&t_end, NULL);
       timeval_subtract(&t_diff, &t_end, &t_start);
-      elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec); 
-
-    printf("before BBBB\n");
+      elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec) / GPU_RUNS; 
 
       float microsecPerMatrixMul = elapsed; 
       double flopsPerMatrixMul = 2.0 * HEIGHT_A * WIDTH_B * WIDTH_A; 
       double gigaFlops = (flopsPerMatrixMul * 1.0e-9f) / (microsecPerMatrixMul / (1000.0f * 1000.0f)); 
 
-      printf("CUBLAS version runs in: %lu microsecs, GFlops/sec: %f\n", elapsed/GPU_RUNS, gigaFlops);
+      printf("CUBLAS version runs in: %lu microsecs, GFlops/sec: %f\n", elapsed, gigaFlops);
 
      // Destroy the handle
      cublasDestroy(handle);
@@ -110,8 +107,6 @@ int main() {
      cudaMalloc(&d_B,nr_rows_B * nr_cols_B * sizeof(float));
      cudaMalloc(&d_C,nr_rows_C * nr_cols_C * sizeof(float));
  
-    printf("before MMM 111\n");
-
      // Fill the arrays A and B on GPU with random numbers
      GPU_fill_rand(d_A, nr_rows_A, nr_cols_A);
      GPU_fill_rand(d_B, nr_rows_B, nr_cols_B);
@@ -124,13 +119,9 @@ int main() {
 //     std::cout << "B =" << std::endl;
 //     print_matrix(h_B, nr_rows_B, nr_cols_B);
  
-     printf("before MMM\n");
-
      // Multiply A and B on GPU
      gpu_blas_mmul_rep(d_A, d_B, d_C, nr_rows_A, nr_cols_A, nr_cols_B);
  
-     printf("after MMM\n");
-
      // Copy (and print) the result on host memory
      cudaMemcpy(h_C,d_C,nr_rows_C * nr_cols_C * sizeof(float),cudaMemcpyDeviceToHost);
 //     std::cout << "C =" << std::endl;
